7f045c604116b29b3044eb0313873e5d6b1c548a
Anibal Sanchez <anibal.sanchez@extly.com>
2024-11-01 14:54:48 +0100

    ci(build): Update script

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,7 @@ echo "Configuring app environment ..."
 pip3 install -r requirements.txt
 python3 -m nltk.downloader wordnet
 python3 -m nltk.downloader punkt
+python3 -m nltk.downloader punkt_tab
 
 # Define env variables DATA_URL and MODELS_URL
 # export DATA_URL="..."
